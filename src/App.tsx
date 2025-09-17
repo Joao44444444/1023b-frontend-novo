4593a58cf0ee6e48129d847b67acba5ab2b8968e
João <joao.costa7@estudante.ifms.edu.br>
2025-09-17 13:00:41 -0300

    arrumando

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+
 type ProdutoType = {
   _id: number;
   nome: string;
@@ -7,18 +8,43 @@ type ProdutoType = {
   urlfoto: string;
   descricao: string;
 }
-function App() {
-  const [produtos, setProdutos] = useState<ProdutoType[]>([])
-  useEffect(() => {
-    fetch('/api/produtos')
-      .then(response => response.json())
-      .then(data => setProdutos(data))
-  }, [])
+
+export default function App() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([]);
+  const handleForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    fetch('/api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+
+    })
+      .then((response) => response.json())
+      .then((NewProduto) => setProdutos([...produtos, NewProduto]))
+      .catch((error) => console.error('Error posting data:', error))
+    form.reset();
+
+  }
 
   return (
     <>
       <div>
-        <h1>TERE</h1>
+        <div>Cadastro de produtos</div>
+        <form onSubmit={handleForm}>
+          <input type="text" name="nome" placeholder="Nome" />
+          <input type="number" name="preco" placeholder="Preço" />
+          <input type="text" name="urlfoto" placeholder="URL da foto" />
+          <input type="text" name="descricao" placeholder="Descrição" />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <h1>Lista de produtos</h1>
       </div>
       {
         produtos.map((produto) => (
@@ -31,6 +57,4 @@ function App() {
         ))}
     </>
   )
-
 }
-export default App
